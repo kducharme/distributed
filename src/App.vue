82321a27e90f855767d21da191084a09e7b45ad9
Kyle Ducharme <kyle@kyles-mbp-2.lan>
2018-10-13 16:29:52 -0500

    finished pagE

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,16 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+}
+
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="hello">
-    <h1>Distributed UX</h1>
+  <div class="content">
+    <h1>I'm Kyle, a UX designer & developer that focuses on <span>b2b software, startups, and remote work.</span> I've helped ship products used by more than 30,000 businesses, and I'm currently working at TaxJar => a fully remote company building software that makes eCommerce easier for merchants and developers.</h1>
+    <a href="https://dribbble.com/kducharme" target="blank">View my portfolio</a>
   </div>
 </template>
 
@@ -15,18 +16,52 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+h1, h2, h3, h4, p, a {
+  font-family: 'Lato';
+  // color: #13293D;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.content {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+
+  h1 {
+    padding: 0 15%;
+    font-weight: 600;
+    font-size: 20px;
+    margin-bottom: 16px;
+    line-height: 1.6;
+    color: #6C7A86;
+    margin-bottom: 100px;
+  }
+
+  span {
+    color: #13293D;
+    font-weight: 40px;
+    opacity: 1!important;
+    font-weight: 900;
+  }
+
+  a {
+    color: #003bde;
+    text-decoration: none;
+    padding: 6px 12px;
+    font-weight: 700;
+  }
+
+  a:hover {
+      color: rgb(60, 110, 248);
+  }
+
+  a:active {
+    background-color: #F0F0F0;
+    border-radius: 5px;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
